a597daa3139e47872661d6a453dbc3687e711077
unknown <anandmanash321@gmail.com>
2024-05-14 15:53:20 +0530

    added image upload in cloudinary

--- app/(Admin)/CreateProduct/page.jsx
+++ app/(Admin)/CreateProduct/page.jsx
@@ -19,6 +19,7 @@ import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import BackButton from "@/components/Custom/BackButton";
 import { Controller, useForm } from "react-hook-form";
+import { uploadImage } from "@/lib/uploadImage";
 
 const CreateProduct = () => {
   const { data: session } = useSession({
@@ -79,16 +80,21 @@ const CreateProduct = () => {
     { value: "sale", label: "Sale" },
     { value: "best-seller", label: "Best Seller" },
     { value: "limited-edition", label: "Limited Edition" },
-    { value: "exclusive", label: "Exclusive" }
+    { value: "exclusive", label: "Exclusive" },
   ];
-  
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    const image = data.image[0];
+    // console.log(process.env.NEXT_PUBLIC_CLOUDINARY_USERNAME);
+
+    const imageUrl = uploadImage(image);
+    console.log(imageUrl);
+  };
 
   return (
     <>
-      <form
-        className=" py-1 bg-blueGray-50"
-        onSubmit={handleSubmit((data) => console.log(data))}
-      >
+      <form className=" py-1 bg-blueGray-50" onSubmit={handleSubmit(onSubmit)}>
         <div className="w-full lg:w-8/12 px-4 mx-auto mt-6 bg-light_prm dark:bg-dark_prm">
           <div className="max-w-4xl mx-auto p-6 md:p-10 space-y-6">
             <BackButton />
@@ -100,10 +106,13 @@ const CreateProduct = () => {
                   <Input
                     id="title"
                     placeholder="Enter product title"
-                    {...register("title",{minLength:2,required:true})}
+                    {...register("title", { minLength: 2, required: true })}
                   />
-                    {errors.title  && (
-                    <p className="errorMsg text-red-500 mt-2">This is a required field and title length should be greater than 2.</p>
+                  {errors.title && (
+                    <p className="errorMsg text-red-500 mt-2">
+                      This is a required field and title length should be
+                      greater than 2.
+                    </p>
                   )}
                 </div>
                 <div>
@@ -112,10 +121,16 @@ const CreateProduct = () => {
                     className="min-h-[120px]"
                     id="description"
                     placeholder="Enter product description"
-                    {...register("description",{minLength:2,required:true})}
+                    {...register("description", {
+                      minLength: 2,
+                      required: true,
+                    })}
                   />
-                     {errors.description  && (
-                    <p className="errorMsg text-red-500 mt-2">This is a required field and description length should be greater than 2.</p>
+                  {errors.description && (
+                    <p className="errorMsg text-red-500 mt-2">
+                      This is a required field and description length should be
+                      greater than 2.
+                    </p>
                   )}
                 </div>
 
@@ -125,10 +140,17 @@ const CreateProduct = () => {
                     id="price"
                     placeholder="Enter product price"
                     type="number"
-                    {...register("price", { valueAsNumber: true,min:0,required:true })}
+                    {...register("price", {
+                      valueAsNumber: true,
+                      min: 0,
+                      required: true,
+                    })}
                   />
-                   {errors.price  && (
-                    <p className="errorMsg text-red-500 mt-2">This is a required field and price should be greater than 0.</p>
+                  {errors.price && (
+                    <p className="errorMsg text-red-500 mt-2">
+                      This is a required field and price should be greater than
+                      0.
+                    </p>
                   )}
                 </div>
                 <div>
@@ -138,7 +160,14 @@ const CreateProduct = () => {
                     control={control}
                     rules={{ required: true }}
                     render={({ field }) => (
-                      <Select {...field} options={categories} />
+                      <Select
+                        classNames={{
+                          control: () => "border border-gray-300 rounded-md",
+                        }}
+                        {...field}
+                        isSearchable
+                        options={categories}
+                      />
                     )}
                   />
                   {errors.categories && (
@@ -150,10 +179,12 @@ const CreateProduct = () => {
                   <Input
                     id="brand"
                     placeholder="Enter brand name"
-                    {...register("brand",{required:true})}
+                    {...register("brand", { required: true })}
                   />
-                   {errors.brand  && (
-                    <p className="errorMsg text-red-500 mt-2">This is a required field</p>
+                  {errors.brand && (
+                    <p className="errorMsg text-red-500 mt-2">
+                      This is a required field
+                    </p>
                   )}
                 </div>
                 <div>
@@ -162,32 +193,48 @@ const CreateProduct = () => {
                     id="inventory"
                     placeholder="Enter inventory count"
                     type="number"
-                    {...register("inventory",{valueAsNumber:true,min:0})}
+                    {...register("inventory", { valueAsNumber: true, min: 0 })}
                   />
-                  {errors.inventory  && (
-                    <p className="errorMsg text-red-500 mt-2">This is a required field and minm should be greater than 0.</p>
+                  {errors.inventory && (
+                    <p className="errorMsg text-red-500 mt-2">
+                      This is a required field and minm should be greater than
+                      0.
+                    </p>
                   )}
                 </div>
                 <div>
                   <Label htmlFor="image-url">Image URL</Label>
-                  <Input id="image-url" placeholder="Enter image URL" />
+                  <input
+                    {...register("image")}
+                    className="relative mt-4 m-0 block w-full min-w-0 flex-auto rounded border cursor-pointer border-solid border-blue-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-blue-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-blue-100 file:px-3 file:py-[0.32rem] file:text-blue-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-blue-700 hover:file:text-white focus:border-blue focus:text-blue-700 focus:shadow-text-blue focus:outline-none dark:border-blue-600 dark:text-blue-200 dark:file:bg-blue-700 dark:file:text-blue-100 dark:focus:border-blue"
+                    type="file"
+                    id="formFile"
+                  />
                 </div>
                 <div className="flex items-center space-x-2">
-                  <input type="checkbox" id="featured" {...register("featured")}/>
-                  <Label htmlFor="featured" >
-                    Is Featured
-                  </Label>
-                  {errors.featured  && (
-                    <p className="errorMsg text-red-500 mt-2">This is a required field .</p>
+                  <input
+                    type="checkbox"
+                    id="featured"
+                    {...register("featured")}
+                  />
+                  <Label htmlFor="featured">Is Featured</Label>
+                  {errors.featured && (
+                    <p className="errorMsg text-red-500 mt-2">
+                      This is a required field .
+                    </p>
                   )}
                 </div>
                 <div className="flex items-center space-x-2">
-                  <input type="checkbox" id="available" {...register("available")}/>
-                  <Label htmlFor="available" >
-                    Is Available
-                  </Label>
-                  {errors.available  && (
-                    <p className="errorMsg text-red-500 mt-2">This is a required field </p>
+                  <input
+                    type="checkbox"
+                    id="available"
+                    {...register("available")}
+                  />
+                  <Label htmlFor="available">Is Available</Label>
+                  {errors.available && (
+                    <p className="errorMsg text-red-500 mt-2">
+                      This is a required field{" "}
+                    </p>
                   )}
                 </div>
               </div>
@@ -198,10 +245,16 @@ const CreateProduct = () => {
                     id="ratings"
                     placeholder="Enter ratings"
                     type="number"
-                    {...register("ratings",{valueAsNumber:true,max:5,min:0})}
+                    {...register("ratings", {
+                      valueAsNumber: true,
+                      max: 5,
+                      min: 0,
+                    })}
                   />
-                   {errors.ratings  && (
-                    <p className="errorMsg text-red-500 mt-2">This is a required field and maxm lte 5 and gte 0 </p>
+                  {errors.ratings && (
+                    <p className="errorMsg text-red-500 mt-2">
+                      This is a required field and maxm lte 5 and gte 0{" "}
+                    </p>
                   )}
                 </div>
                 {/* <div>
@@ -247,11 +300,13 @@ const CreateProduct = () => {
                     control={control}
                     rules={{ required: true }}
                     render={({ field }) => (
-                      <Select {...field} isMulti options={tags} />
+                      <Select {...field} isMulti isSearchable options={tags} />
                     )}
                   />
-                  {errors.tags  && (
-                    <p className="errorMsg text-red-500 mt-2">This is a required field.</p>
+                  {errors.tags && (
+                    <p className="errorMsg text-red-500 mt-2">
+                      This is a required field.
+                    </p>
                   )}
                 </div>
                 <div>
@@ -262,11 +317,13 @@ const CreateProduct = () => {
                     control={control}
                     rules={{ required: true }}
                     render={({ field }) => (
-                      <Select {...field} options={sizes} />
+                      <Select {...field} isSearchable options={sizes} />
                     )}
                   />
                   {errors.sizes && (
-                    <p className="errorMsg text-red-500 mt-2">This is a required field.</p>
+                    <p className="errorMsg text-red-500 mt-2">
+                      This is a required field.
+                    </p>
                   )}
                 </div>
                 <div>
@@ -276,11 +333,13 @@ const CreateProduct = () => {
                     control={control}
                     rules={{ required: true }}
                     render={({ field }) => (
-                      <Select {...field} options={colors} />
+                      <Select {...field} isSearchable options={colors} />
                     )}
                   />
                   {errors.colors && (
-                    <p className="errorMsg text-red-500 mt-2">This is a required field.</p>
+                    <p className="errorMsg text-red-500 mt-2">
+                      This is a required field.
+                    </p>
                   )}
                 </div>
                 {/* <div>
